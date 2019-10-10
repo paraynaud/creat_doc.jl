3cc05b50cd85b403a5e900c17561d201495bcb8a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-10 14:24:26 +0000

    build based on 4b586dd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#creat_doc.jl-Documentation-1","page":"creat_doc.jl Documentation","title":"creat_doc.jl Documentation","text":"","category":"section"},{"location":"#","page":"creat_doc.jl Documentation","title":"creat_doc.jl Documentation","text":"greet()","category":"page"},{"location":"#Main.hello_world.greet-Tuple{}","page":"creat_doc.jl Documentation","title":"Main.hello_world.greet","text":"greet()\n\naffiche Hello World!\n\n\n\n\n\n","category":"method"}]
+}
